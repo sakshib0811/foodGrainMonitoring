210fb99dfa6c0154c3300d4a1c7c7aec6b4bd493
sakshib0811 <85578492+sakshib0811@users.noreply.github.com>
2022-07-29 00:35:50 +0530

    team details added

--- dashboard/agroapp/src/Components/About.js
+++ dashboard/agroapp/src/Components/About.js
@@ -2,13 +2,17 @@ import React from "react";
 import "./About.css";
 import { Link } from "react-router-dom";
 import img1 from "./images/image.jpeg";
+import img2 from "./images/img22.jpeg";
+import img3 from "./images/img3.jpeg";
+import img4 from "./images/img4.jpeg";
+import img5 from "./images/img55.jpeg";
 function About() {
   return (
     <div>
       <nav className="main-nav">
         <div className="logo">
           <Link to="/" style={{ textDecoration: "none", color: "white" }}>
-            <h2 className="title">FOOD MONITORING APP</h2>
+            <h2 className="title_main">FOOD MONITORING APP</h2>
           </Link>
         </div>
       </nav>
@@ -30,11 +34,95 @@ function About() {
       </p>
       <h1 className="mission">OUR GUIDE</h1>
       <div className="guide_detail">
-        <img src={img1} alt="guide image" className="guide_img" />
-        <h1 style={{ marginBottom: "0px" }}>Dr. Padmaja K.V.</h1>
-        <p style={{ marginTop: "auto" }}>Associate Professor E&I Department </p>
+        <div class="column1">
+          <div class="card card1">
+            <img
+              src={img1}
+              alt="Guide"
+              style={{ width: "100%" }}
+              className="guide_img"
+            />
+            <div class="container">
+              <h2>Dr. Padmaja K.V.</h2>
+              <p class="title">Guide</p>
+              <p>Associate Professor of E&I Department, RVCE</p>
+              <p>padmajakv@rvce.edu.in</p>
+              <p>
+                <button class="button">Contact</button>
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* <img src={img1} alt="guide image" className="guide_img" /> */}
+        {/* <h1 style={{ marginBottom: "0px" }}>Dr. Padmaja K.V.</h1>
+        <p style={{ marginTop: "auto" }}>Associate Professor E&I Department </p> */}
       </div>
-      <h1 className="mission">OUR TEAM</h1>
+
+      <h1 className="mission member">OUR TEAM</h1>
+
+      <div className="row">
+        <div class="column">
+          <div class="card">
+            <img src={img2} alt="Aman" style={{ width: "100%" }} />
+            <div class="container">
+              <h2>Aman Kumar Singh</h2>
+              <p class="title">Team Member</p>
+              <p>3rd year student, EIE, RVCE</p>
+              <p>amankumar.ei19@rvce.edu.in</p>
+              <p>
+                <button class="button">Contact</button>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="column">
+          <div class="card">
+            <img src={img3} alt="Sakshi" style={{ width: "100%" }} />
+            <div class="container">
+              <h2>Sakshi Bharti</h2>
+              <p class="title">Team Member</p>
+              <p>3rd year student, EIE, RVCE</p>
+              <p>sakshibharti.ei19@rvce.edu.in</p>
+              <p>
+                <button class="button">Contact</button>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="column">
+          <div class="card">
+            <img src={img4} alt="Samiya" style={{ width: "100%" }} />
+            <div class="container">
+              <h2>Samiya Goyal</h2>
+              <p class="title">Team Member</p>
+              <p>3rd year student, EIE, RVCE</p>
+              <p>samiyagoyal.ei19@rvce.edu.in</p>
+              <p>
+                <button class="button">Contact</button>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="column">
+          <div class="card">
+            <img src={img5} alt="Pankaj" style={{ width: "100%" }} />
+            <div class="container">
+              <h2>Pankaj Kankani</h2>
+              <p class="title">Team Member</p>
+              <p>3rd year student, EIE, RVCE</p>
+              <p>pankajkankani.ei19@rvce.edu.in</p>
+              <p>
+                <button class="button">Contact</button>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* main */}
     </div>
   );
 }
